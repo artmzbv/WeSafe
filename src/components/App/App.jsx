4ed120ea37ad564709ac6dbcee2415915318532f
Artem Zubov <artmzbv@gmail.com>
2024-02-07 18:10:29 +0300

    template

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,12 +3,14 @@ import './App.css';
 import Header from "../Header/Header"
 import Footer from "../Footer/Footer"
 import Main from "../Main/Main"
+import Offer from "../Offer/Offer"
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Main/>
+      <Offer/>
       <Footer/>
     </div>
   );
---
+++ src/components/Offer/Offer.jsx
@@ -0,0 +1,11 @@
+import "./Offer.css"
+
+export default function Offer() {
+    return(
+        <>
+        <section>
+            <div>Offer</div>
+        </section>
+        </>
+    )
+}
