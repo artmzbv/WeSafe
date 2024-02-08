7a6696763ce454e9bbedb6e87d8f4e8cb2b6f6b8
Artem Zubov <artmzbv@gmail.com>
2024-02-08 17:32:21 +0300

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,34 @@
 import {React, useState}  from "react";
 import { Route, Link, Switch } from "react-router-dom";
 import headerLogo from "../../images/logo.jpg"
+import { ReactComponent as Triangular } from "../../images/triangular.svg"
 import './Header.css';
 
 // https://www.google.com/search?q=position+for+dropdown+list
 export default function Header() {
     const [openMetals, setOpenMetals] = useState(false);
     const handleOpenMetals = (e) =>{
-        setOpenMetals(!openMetals);
-        e.preventDefault(); 
+        setOpenMetals(true);
+        e.preventDefault();  
     }
+
+    const handleCloseMetals =(e)=>{
+        setOpenMetals(false);
+        e.preventDefault();
+    }
+
     return(
         <>
         <header className='header'>
-        <img className="header__logo" src={headerLogo} alt="Место" />
+        <img className="header__logo" src={headerLogo} alt="logo" />
             <div className="header__links">
             {/* <Route exact path="/center"> */}
             <Link className="header__link" to="/offer">
                 Notre Offre
             </Link>
-                <button className ="header__link header__link_button" onClick={handleOpenMetals}>Nos Metaux \/</button>
+                <button className ="header__link header__link_button" onMouseEnter={handleOpenMetals} onMouseLeave={handleCloseMetals}>Nos Metaux&#11206;
                 {openMetals ? (
+                    <nav className="header__dropdown">
                         <ul className="header__dropdown-list">
                             <li className="header__dropdown-item">
                                   <button className='header__dropdown-button' onClick={() => {setOpenMetals(false)}}>
@@ -44,7 +52,9 @@ export default function Header() {
                                   </button>
                             </li>
                             </ul>
+                            </nav>
                 ) : null}
+                </button>
             <Link className="header__link" to="/insights">
                 Insights
             </Link>
