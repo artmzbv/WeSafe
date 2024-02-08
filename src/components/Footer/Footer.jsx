c39b629f349e937f38f9abac950878d581c9859f
Artem Zubov <artmzbv@gmail.com>
2024-02-08 18:53:35 +0300

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,40 @@
+import {Link} from "react-router-dom";
 import './Footer.css';
 
 export default function Footer() {
     return(
         <>
         <footer className='footer'>
-            <div>Footer</div>
+        <Link className='footer__link footer__link_title' to="./offer">
+            Notre Offre
+        </Link>
+            <ul className='footer__list'>
+            <li className='footer__link footer__link_title'>Metaux</li>
+            <li className="footer__list-item">
+            <Link className='footer__link footer__link_subtitle' to="./copper">
+                Cuivre
+            </Link>
+            </li>
+            <li className="footer__list-item">
+            <Link className='footer__link footer__link_subtitle' to="./aluminium">
+                Aluminium
+            </Link>
+            </li>
+            <li className="footer__list-item">
+            <Link className='footer__link footer__link_subtitle' to="./nickel">
+                Nickel
+            </Link>
+            </li>
+            </ul>
+        <Link className='footer__link footer__link_title' to="./insights">
+            <div className='footer__link'>Insights</div>
+        </Link>
+        <Link className='footer__link footer__link_title' to="./about-us">
+            <div className='footer__link'>Qui sommes-nous?</div>
+        </Link>
+        <Link className='footer__link footer__link_title' to="./contact-us">
+            <div className='footer__link'>Contactez-nous</div>
+        </Link>  
         </footer>
         </>
     )
