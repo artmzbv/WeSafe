5136346222030689306e80ffdc1d5b2ee10e240c
Artem Zubov <artmzbv@gmail.com>
2024-02-11 20:55:57 +0300

    insights

--- src/components/Insights/Insights.jsx
+++ src/components/Insights/Insights.jsx
@@ -8,7 +8,13 @@ export default function Insights() {
             <h1 className="insights__title">Nouvelles&Insights</h1>
                 <h2 className="insights__subtitle">Suivez  les dernières mises à jour sur le marché avec nous !</h2>
             </div>
-            <div className='insights__container'></div>
+            <div className='insights__container'>
+            <div className='insights__buttons'>    
+                <button className='insights__button'>Tout ensemble</button>
+                <button className='insights__button'>Insights</button>
+                <button className='insights__button'>Nouvelles</button>
+            </div>
+            </div>
         </section>
         </>
     )
---
+++ src/components/InsightsCard/InsightsCard.jsx
@@ -0,0 +1,15 @@
+import './InsightsCard.css';
+
+export default function InsightsCard() {
+    return(
+        <>
+        <section className="insights">
+            <div className="insights__topic">
+            <h1 className="insights__title">Nouvelles&Insights</h1>
+                <h2 className="insights__subtitle">Suivez  les dernières mises à jour sur le marché avec nous !</h2>
+            </div>
+            <div className='insights__container'></div>
+        </section>
+        </>
+    )
+}
