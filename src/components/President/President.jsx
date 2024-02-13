1522f1da24290d6d88f201f1d348190143bf1561
Artem Zubov <artmzbv@gmail.com>
2024-02-13 20:28:26 +0300

    aboutus_titles

--- src/components/President/President.jsx
+++ src/components/President/President.jsx
@@ -5,9 +5,12 @@ export default function President() {
     return(
         <>
         <section className='president'>
-            <h1 className='president__title'>Le mot du President</h1>
             <div className='president__container'>
+                <div className=''>
                 <img className='president__photo' src={jerome} alt='president'></img>
+                <p className='president__name'>Jerome Thomasse</p>
+                <p className='president__profession'>President de WeSafe</p>
+                </div>
                 <p className='president__description'>
                 Je vous prie Mesdames et Messieurs, mes salutations distinguees Je vous prie Mesdames et Messieurs, mes salutations distinguees 
                 Je vous prie Mesdames et Messieurs, mes salutations distinguees Je vous prie Mesdames et Messieurs, mes salutations distinguees 
