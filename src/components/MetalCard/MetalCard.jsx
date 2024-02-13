3861d6721eb267f853ea07492ff046986cc7e1fd
Artem Zubov <artmzbv@gmail.com>
2024-02-13 12:52:10 +0300

    metal_card_mouseover

--- src/components/MetalCard/MetalCard.jsx
+++ src/components/MetalCard/MetalCard.jsx
@@ -4,24 +4,30 @@ import MetalPopup from '../MetalPopup/MetalPopup';
 
 export default function MetalCard({card, handleOpenPopup}) {
     const [popup, setPopup] = useState(false);
+    const [hover, setHover] = useState(false)
     // const cards=['']
     function handleOpenPopup() {
         setPopup(true);
     }
+
     function handleClosePopup() {
         setPopup(false);
     }
+
+    
     return(
         <>
     {popup ? (<MetalPopup card={card} handleClosePopup={handleClosePopup} />) : null}
         <section className="types" onClick={()=> handleOpenPopup()}>
-        <h1 className="types__name">{card.title}</h1>
+        <h1 className="types__name" onMouseOver={()=>setHover(true)} onMouseOut={()=>setHover(false)}>{card.title}</h1>
             <img 
-            className="types__images"
+            className={`types__images  ${hover ? 'types__images_hover' : null}`}
+            onMouseOver={()=>setHover(true)}
+            onMouseOut={()=>setHover(false)}
             src={card.link}
             alt={card.name}
             />
-            <h1 className="types__name">{card.title}</h1>
+            {/* <h1 className="types__name">{card.title}</h1> */}
         </section>
         </>
     )
