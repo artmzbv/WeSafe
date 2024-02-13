516c977e58e54d9385f9de276f308c05939c47a7
Artem Zubov <artmzbv@gmail.com>
2024-02-13 21:29:35 +0300

    offer

--- src/components/Offer/Offer.jsx
+++ src/components/Offer/Offer.jsx
@@ -4,10 +4,11 @@ import image from '../../images/offer_copper.jpg'
 export default function Offer() {
     return(
         <>
-        <section className="offer">
+            <section className="offer">
             <div className="offer__topic">
                 <h1 className="offer__title">Dès le premier trimestre 2024, vous pourrez accéder à notre service achat de CUIVRE, qui vous permettra</h1>
             </div>
+            <div className="offer__container">
             <div className="offer__card">
                 <div className="offer__card-image"></div>
                 <h1 className="offer__card-title">D’accéder à un marché normalement réservé aux industriels</h1>
@@ -27,6 +28,7 @@ export default function Offer() {
                 <div className="offer__card-image"></div>
                 <h1 className="offer__card-title">D’offrir un placement de « bon père de famille »,</h1>
                 <p className="offer__card-description"> en transformant une partie de l’argent placé dans les banques exposée aux risques financiers, en matière première physique à fort potentiel</p>
+            </div>
             </div>
         </section>
         </>
