a8c11bd1b7945bdfcbd812dc7e800b9f372576ca
Artem Zubov <artmzbv@gmail.com>
2024-02-14 15:55:27 +0300

    main_pointer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -27,7 +27,9 @@ export default function Header() {
             <Link className="header__link" to="/offer">
                 Notre Offre
             </Link>
-                <button className ="header__link header__link_button" onMouseEnter={handleOpenMetals} onMouseLeave={handleCloseMetals}>Nos Metaux&#11206;
+            {/* &#11206; */}
+                <button className ="header__link header__link_button" onMouseEnter={handleOpenMetals} onMouseLeave={handleCloseMetals}>Nos Metaux&nbsp;&nbsp;
+                <div className={`${openMetals ? 'header__active-pointer' : 'header__passive-pointer' }`}> </div>
                 {openMetals ? (
                     <nav className="header__dropdown">
                         <ul className="header__dropdown-list">
