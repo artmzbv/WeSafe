1b4211e43ad39d38af4e2daa5da98c2500c038ad
Artem Zubov <artmzbv@gmail.com>
2024-02-14 18:14:55 +0300

    header_underline

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,13 @@ import './Header.css';
 // https://www.google.com/search?q=position+for+dropdown+list
 export default function Header() {
     const [openMetals, setOpenMetals] = useState(false);
+    const[active, setActive] = useState("");
+
+    const handleActiveClick = (e) => {
+        // event.preventDefault()
+        setActive(e.target.id);
+      }
+
     const handleOpenMetals = (e) =>{
         setOpenMetals(true);
         e.preventDefault();  
@@ -20,37 +27,37 @@ export default function Header() {
         <>
         <header className='header'>
             <Link to="/">
-            <img className="header__logo" src={headerLogo} alt="logo" />
+            <img id={"0"} className="header__logo" src={headerLogo} alt="logo" onClick={handleActiveClick} />
             </Link>
             <div className="header__links">
             {/* <Route exact path="/center"> */}
-            <Link className="header__link" to="/offer">
+            <Link id={"1"} className={`header__link ${active === "1" ? "header__link_active" : null}`}  to="/offer" onClick={handleActiveClick} >
                 Notre Offre
             </Link>
             {/* &#11206; */}
-                <button className ="header__link header__link_button " onMouseEnter={handleOpenMetals} onMouseLeave={handleCloseMetals}>Nos Metaux&nbsp;&nbsp;&nbsp;
+                <button className={`header__link header__link_button ${active === "2" ? "header__link_active" : "header__link_button-passive"}`} onMouseEnter={handleOpenMetals} onMouseLeave={handleCloseMetals}>Nos Metaux&nbsp;&nbsp;&nbsp;
                 {/* <div className={`${openMetals ? 'header__active-pointer' : 'header__passive-pointer' }`}> </div> */}
                 {/* <div className='header__pointer'> </div> */}
                 {openMetals ? (
                     <nav className="header__dropdown">
                         <ul className="header__dropdown-list">
                             <li className="header__dropdown-item">
-                                  <button className='header__dropdown-button' onClick={() => {setOpenMetals(false)}}>
-                                    <Link className="header__link" to="/copper">
+                                  <button className='header__dropdown-button' onClick={(e) => {setOpenMetals(false); handleActiveClick(e)}}>
+                                    <Link id={"2"} className="header__link" to="/copper">
                                     Copper
                                     </Link>
                                   </button>
                             </li>
                             <li className="header__dropdown-item">
-                                  <button className='header__dropdown-button' onClick={() => {setOpenMetals(false)}}>
-                                    <Link className="header__link" to="/aluminium">
+                                  <button className='header__dropdown-button' onClick={(e) => {setOpenMetals(false); handleActiveClick(e)}}>
+                                    <Link id={"2"} className="header__link" to="/aluminium">
                                     Alumnium
                                     </Link>
                                   </button>
                             </li>
                             <li className="header__dropdown-item">
-                                  <button className='header__dropdown-button' onClick={() => {setOpenMetals(false)}}>
-                                    <Link className="header__link" to="/nickel">
+                                  <button className='header__dropdown-button' onClick={(e) => {setOpenMetals(false); handleActiveClick(e)}}>
+                                    <Link id={"2"} className="header__link" to="/nickel">
                                     Nickel
                                     </Link>
                                   </button>
@@ -59,13 +66,13 @@ export default function Header() {
                             </nav>
                 ) : null}
                 </button>
-            <Link className="header__link" to="/insights">
+            <Link id={"3"} className={`header__link ${active === "3" ? "header__link_active" : null}`} onClick={(e) => {handleActiveClick(e)}} to="/insights">
                 Insights
             </Link>
-            <Link className="header__link" to="/about-us">
+            <Link id={"4"} className={`header__link ${active === "4" ? "header__link_active" : null}`} onClick={(e) => {handleActiveClick(e)}} to="/about-us">
                 Qui sommes-nous?
             </Link>
-            <Link className="header__link" to="/contact-us">
+            <Link id={"5"} className={`header__link ${active === "5" ? "header__link_active" : null}`} onClick={(e) => {handleActiveClick(e)}} to="/contact-us">
                 Contactez-nous
             </Link>
         </div>
