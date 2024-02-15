8ba532b2cc1462a5fa179a757af4fadf07ed88e2
Artem Zubov <artmzbv@gmail.com>
2024-02-15 18:49:12 +0300

    burger_menu_divided_components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,32 +2,19 @@ import {React, useState}  from "react";
 import { Link } from "react-router-dom";
 import headerLogo from "../../images/logo_new.jpg"
 import './Header.css';
-import BurgerMenu from "../BurgerMenu/BurgerMenu"
+import Navigation from '../Navigation/Navigation'
 
 // https://www.google.com/search?q=position+for+dropdown+list
 export default function Header() {
     const [isShowMenu, setIsShowMenu] = useState(false);
-    const [openMetals, setOpenMetals] = useState(false);
-
-    const[active, setActive] = useState("");
+    const [active, setActive] = useState("");
     const handleActiveClick = (e) => {
-        // event.preventDefault()
         setActive(e.target.id);
       }
 
-    const handleOpenMetals = (e) =>{
-        setOpenMetals(true);
-        e.preventDefault();  
+    const handleToggleBurgerMenu = () =>{
+        setIsShowMenu(!isShowMenu);
     }
-
-    const handleCloseMetals =(e)=>{
-        setOpenMetals(false);
-        e.preventDefault();
-    }
-    // const handleOpenBurgerMenu = () =>{
-    //     setIsShowMenu(true);
-    // }
-    console.log(!isShowMenu)
 
     return(
         <>
@@ -35,55 +22,20 @@ export default function Header() {
             <Link to="/">
             <img id={"0"} className="header__logo" src={headerLogo} alt="logo" onClick={handleActiveClick} />
             </Link>
-            <div className="header__links">
-            {/* <Route exact path="/center"> */}
-            <Link id={"1"} className={`header__link ${active === "1" ? "header__link_active" : null}`}  to="/offer" onClick={handleActiveClick} >
-                Notre Offre
-            </Link>
-            {/* &#11206; */}
-                <button className={`header__link header__link_button ${active === "2" ? "header__link_active" : "header__link_button-passive"}`} onMouseEnter={handleOpenMetals} onMouseLeave={handleCloseMetals}>Nos Metaux&nbsp;&nbsp;&nbsp;
-                {/* <div className={`${openMetals ? 'header__active-pointer' : 'header__passive-pointer' }`}> </div> */}
-                {/* <div className='header__pointer'> </div> */}
-                {openMetals ? (
-                    <nav className="header__dropdown">
-                        <ul className="header__dropdown-list">
-                            <li className="header__dropdown-item">
-                                  <button className='header__dropdown-button' onClick={(e) => {setOpenMetals(false); handleActiveClick(e)}}>
-                                    <Link id={"2"} className="header__link" to="/copper">
-                                    Copper
-                                    </Link>
-                                  </button>
-                            </li>
-                            <li className="header__dropdown-item">
-                                  <button className='header__dropdown-button' onClick={(e) => {setOpenMetals(false); handleActiveClick(e)}}>
-                                    <Link id={"2"} className="header__link" to="/aluminium">
-                                    Alumnium
-                                    </Link>
-                                  </button>
-                            </li>
-                            <li className="header__dropdown-item">
-                                  <button className='header__dropdown-button' onClick={(e) => {setOpenMetals(false); handleActiveClick(e)}}>
-                                    <Link id={"2"} className="header__link" to="/nickel">
-                                    Nickel
-                                    </Link>
-                                  </button>
-                            </li>
-                            </ul>
-                            </nav>
-                ) : null}
-                </button>
-            <Link id={"3"} className={`header__link ${active === "3" ? "header__link_active" : null}`} onClick={(e) => {handleActiveClick(e)}} to="/insights">
-                Insights
-            </Link>
-            <Link id={"4"} className={`header__link ${active === "4" ? "header__link_active" : null}`} onClick={(e) => {handleActiveClick(e)}} to="/about-us">
-                Qui sommes-nous?
-            </Link>
-            <Link id={"5"} className={`header__link ${active === "5" ? "header__link_active" : null}`} onClick={(e) => {handleActiveClick(e)}} to="/contact-us">
-                Contactez-nous
-            </Link>
-        </div>
-        <button className = {`${isShowMenu ? 'header__burger-close-button' : 'header__burger-menu' }`} onClick={()=>setIsShowMenu(!isShowMenu)}></button>
-        {isShowMenu ? <BurgerMenu/> : null}
+            {!isShowMenu ?
+            <>
+            <div className="header__links-row">
+            <Navigation active={active} handleActiveClick={handleActiveClick} />
+            </div>
+            </>
+            : 
+            <div className="header__links-column">
+            <div  className="header__links-column-container">
+            <Navigation isShowMenu={isShowMenu} handleToggleBurgerMenu={handleToggleBurgerMenu} setIsShowMenu={setIsShowMenu} active={active} handleActiveClick={handleActiveClick} />
+            </div>
+            </div>
+            }
+        <button className = {`${isShowMenu ? 'header__burger-close-button' : 'header__burger-menu' }`} onClick={()=>handleToggleBurgerMenu()}></button>
         </header>
         </>
     )
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,69 @@
+import {React, useState, useEffect} from "react";
+import { Link } from "react-router-dom";
+import "./Navigation.css";
+
+function Navigation({active, handleActiveClick, isShowMenu, setIsShowMenu}) {
+    const [openMetals, setOpenMetals] = useState(false);
+
+    const handleToggleMetals = (e) =>{
+        setOpenMetals(!openMetals);
+        e.preventDefault();  
+    }
+
+    const handleCloseBurgerMenu = () =>{
+        return isShowMenu ? setIsShowMenu(false) : null
+    }
+
+    const handleCloseMetalsBurgerMenu = (e) =>{
+        return isShowMenu ? handleToggleMetals(e) : null 
+    }
+
+
+  return (
+    <>
+    <Link id={"1"} className={`navigation__link ${active === "1" ? "navigation__link_active" : null}`}  to="/offer" onClick={(e) => { handleActiveClick(e); handleCloseBurgerMenu()}}>
+        Notre Offre
+    </Link>
+        <button className={`navigation__link navigation__link_button ${active === "2" ? "navigation__link_active" : "navigation__link_button-passive"}`} onMouseEnter={handleToggleMetals} onMouseLeave={handleToggleMetals} onClick={handleCloseMetalsBurgerMenu}>Nos Metaux&nbsp;&nbsp;&nbsp;
+        {openMetals ? (
+            <nav className="navigation__dropdown">
+                <ul className="navigation__dropdown-list">
+                    <li className="navigation__dropdown-item">
+                          <button className='navigation__dropdown-button' onClick={(e) => {setOpenMetals(false); handleActiveClick(e); handleCloseBurgerMenu()}}>
+                            <Link id={"2"} className="navigation__link" to="/copper">
+                            Copper
+                            </Link>
+                          </button>
+                    </li>
+                    <li className="navigation__dropdown-item">
+                          <button className='navigation__dropdown-button' onClick={(e) => {setOpenMetals(false); handleActiveClick(e); handleCloseBurgerMenu()}}>
+                            <Link id={"2"} className="navigation__link" to="/aluminium">
+                            Alumnium
+                            </Link>
+                          </button>
+                    </li>
+                    <li className="navigation__dropdown-item">
+                          <button className='navigation__dropdown-button' onClick={(e) => {setOpenMetals(false); handleActiveClick(e); handleCloseBurgerMenu()}}>
+                            <Link id={"2"} className="navigation__link" to="/nickel">
+                            Nickel
+                            </Link>
+                          </button>
+                    </li>
+                    </ul>
+                    </nav>
+        ) : null}
+        </button>
+    <Link id={"3"} className={`navigation__link ${active === "3" ? "navigation__link_active" : null}`} onClick={(e) => {handleActiveClick(e); handleCloseBurgerMenu()}} to="/insights">
+        Insights
+    </Link>
+    <Link id={"4"} className={`navigation__link ${active === "4" ? "navigation__link_active" : null}`} onClick={(e) => {handleActiveClick(e); handleCloseBurgerMenu()}} to="/about-us">
+        Qui sommes-nous?
+    </Link>
+    <Link id={"5"} className={`navigation__link ${active === "5" ? "navigation__link_active" : null}`} onClick={(e) => {handleActiveClick(e); handleCloseBurgerMenu()}} to="/contact-us">
+        Contactez-nous
+    </Link>
+    </>
+  );
+}
+
+export default Navigation;
