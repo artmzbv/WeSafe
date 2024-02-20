3cec1b96625f6e57efb36e26197eff7d1279bc0f
Artem Zubov <artmzbv@gmail.com>
2024-02-21 00:12:36 +0300

    main

--- src/components/AboutProduct/AboutProduct.jsx
+++ src/components/AboutProduct/AboutProduct.jsx
@@ -13,21 +13,21 @@ export default function AboutProduct() {
                 <div className='product__card'>
                     <h2 className='product__subtitle'>1.Selection</h2>
                     <img className='product__image' src={selection} alt='selection'></img>
-                    <p className='product__subtitle'>
-                    Vous choisisez et selectionnez seul ou avec un de nos consultants via WesSafe App
+                    <p className='product__description'>
+                    Vous choisisez et selectionnez seul ou avec un de nos consultants via WeSafe App
                     </p>
                 </div>
                 <div className='product__card'>
                     <h2 className='product__subtitle'>2.Commande</h2>
                     <img className='product__image' src={commande} alt='commande'></img>
-                    <p className='product__subtitle'>
+                    <p className='product__description'>
                     Après la réservation la commande est transmise à l'un de nos fournisseurs
                     </p>
                 </div>
                 <div className='product__card'>
-                    <h2 className='product__subtitle'>3.Livraison et Stockage</h2>
+                    <h2 className='product__subtitle product__subtitle_livraison'>3.Livraison et Stockage</h2>
                     <img className='product__image' src={livraison} alt='livraison'></img>
-                    <p className='product__subtitle'>
+                    <p className='product__description'>
                     La commande est tranportée et livrée par notre fournisseur, puis stockée dans l'un de nos entrepôts ultra-sécurisés
                     </p>
                 </div>
@@ -41,7 +41,7 @@ export default function AboutProduct() {
                 <div className='product__card'>
                     <h2 className='product__subtitle'>4.Revente</h2>
                     <img className='product__image' src={stockage} alt='stockage'></img>
-                    <p className='product__subtitle'>
+                    <p className='product__description'>
                     A moyen ou long termes suivant vos objectifs, bénéficier de notre service de revente aupres des industriels
                     </p>
                 </div>
