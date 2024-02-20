0a7ce290a7fac3553b34f4fc9b0424921d2a7916
Artem Zubov <artmzbv@gmail.com>
2024-02-20 20:32:00 +0300

    main

--- src/components/Insights/Insights.jsx
+++ src/components/Insights/Insights.jsx
@@ -29,7 +29,7 @@ export default function Insights() {
         <section className="insights">
             <div className="insights__topic">
             <h1 className="insights__title">Nouvelles&Insights</h1>
-                <h2 className="insights__subtitle">Suivez  les dernières mises à jour sur le marché avec nous !</h2>
+                <h2 className="insights__subtitle">Suivez  les dernières mises à jour sur le marché avec nous</h2>
             </div>
             <div className='insights__container'>
             <div className='insights__buttons'>    
