fb24db91a400cadfe36ef3703a234b9c5b804d42
Artem Zubov <artmzbv@gmail.com>
2024-02-21 17:41:52 +0300

    questions

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -7,7 +7,10 @@ export default function Intro() {
         <>
         <section className='intro'>
             <div className='intro__text'>
-                <h1 className='intro__title'>Leader européen d'épargne en materiaux critiques</h1>  
+                <h1 className='intro__title'>Leader Européen d'Epargne en Materiaux Critiques</h1>  
+                {/* <h1 className='intro__title'>Leader Europeen</h1>
+                <h1 className='intro__title'>d'Epargne</h1>
+                <h1 className='intro__title'>Metaux Critiques</h1> */}
                 <h2 className='intro__description'>Créer du patrmoine hors banques, hors système</h2>
                 <Link to="/contact-us">
                 <button className='intro__button'>Contactez-nous</button>
