6527510b0fbe77b1a72f2c6bd13b10e4ac4b6fad
Artem Zubov <artmzbv@gmail.com>
2024-03-03 18:25:14 +0300

    arrow

--- src/components/AboutMetals/AboutMetals.jsx
+++ src/components/AboutMetals/AboutMetals.jsx
@@ -21,14 +21,13 @@ export default function AboutMetals() {
             {/* <p className='metals__paragraph'>
             Les prix des metaux vont augmenter au cours des 20 prochaines années
             </p> */}
-                <Link className='metals__link-insights' to="/insights">
-                <button className='metals__subtitle'>{'Voyez aussi nos insights sur le marche  '}
-                <svg width="15" height="15" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M1 19L19.5 0.485929" stroke="white" stroke-width="4"/>
-                <path d="M19.0226 15L19.0131 -0.0499983" stroke="white" stroke-width="4"/>
-                <path d="M19.0132 0.973145L5 0.986549" stroke="white" stroke-width="4"/>
-                </svg>
-                </button>
+                <Link className='metals__link-insights metals__subtitle' to="/insights">
+                Voyez aussi nos insights sur le marché &#129133;
+                {/* <svg width="20" height="20" viewBox="0 0 20 20" fill="white" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1 19L19.5 0.485929" stroke-width="4" fill="white"/>
+                <path d="M19.0226 15L19.0131 -0.0499983" stroke-width="4" fill="white"/>
+                <path d="M19.0132 0.973145L5 0.986549" stroke-width="4" fill="white"/>
+                </svg> */}
             </Link>
             </div>
             <TransitionGraph className='metals__graphics'/>
