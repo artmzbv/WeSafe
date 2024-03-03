46e6d3d7a44eec50e264c4dbf3d916a4bad62b5b
Artem Zubov <artmzbv@gmail.com>
2024-03-03 17:32:11 +0300

    arrow

--- src/components/AboutMetals/AboutMetals.jsx
+++ src/components/AboutMetals/AboutMetals.jsx
@@ -22,7 +22,13 @@ export default function AboutMetals() {
             Les prix des metaux vont augmenter au cours des 20 prochaines années
             </p> */}
                 <Link className='metals__link-insights' to="/insights">
-                <button className='metals__subtitle'>Voyez aussi nos insights sur le marche</button>
+                <button className='metals__subtitle'>{'Voyez aussi nos insights sur le marche  '}
+                <svg width="15" height="15" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1 19L19.5 0.485929" stroke="white" stroke-width="4"/>
+                <path d="M19.0226 15L19.0131 -0.0499983" stroke="white" stroke-width="4"/>
+                <path d="M19.0132 0.973145L5 0.986549" stroke="white" stroke-width="4"/>
+                </svg>
+                </button>
             </Link>
             </div>
             <TransitionGraph className='metals__graphics'/>
