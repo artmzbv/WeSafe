350c467eb6a1f579f19901b138a0a09941a0feb0
Artem Zubov <artmzbv@gmail.com>
2024-03-11 16:20:43 +0300

    form

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -1,6 +1,51 @@
+import {React, useState, useRef, useEffect}  from "react";
 import "./ContactUs.css"
 
 export default function ContactUs() {
+    const [inputName, setInputName] = useState("");
+    const [inputSurname, setInputSurname] = useState("");
+    const [inputAdress, setInputAdress] = useState("");
+    const [inputPhone, setInputPhone] = useState("");
+    const [inputMessage, setInputMessage] = useState("");
+
+    const handleInputNameChange = (e) => {
+        setInputName(e.target.value)
+    }
+
+    const handleInputSurnameChange = (e) => {
+        setInputSurname(e.target.value)
+    }
+
+    const handleInputAdressChange = (e) => {
+        setInputAdress(e.target.value)
+    }
+
+    const handleInputPhoneChange = (e) => {
+        setInputPhone(e.target.value)
+    }
+
+    const handleInputMessageChange = (e) => {
+        setInputMessage(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const data ={
+            name: inputName,
+            surname: inputSurname,
+            adress: inputAdress,
+            phone: inputPhone,
+            message: inputMessage
+        }
+        console.log(data)
+        
+        setInputName("")
+        setInputSurname("")
+        setInputAdress("")
+        setInputPhone("")
+        setInputMessage("")
+    } 
+ 
     return(
         <>
         <section className="contact">
@@ -9,16 +54,16 @@ export default function ContactUs() {
         </div>
             <div className="contact__container">  
             <h2 className="contact__text">Si vous-avez des question, envoyez-nous une message par mail a soutien@wesafe.com</h2>
-            <form className="contact__form">
+            <form className="contact__form" onSubmit={handleSubmit}>
                 <div className="contact__form-line">
-                <input className="contact__input" placeholder="Prenom"></input>
-                <input className="contact__input" placeholder="Nom"></input>
+                <input className="contact__input" placeholder="Prenom" value={inputName} onChange={handleInputNameChange}></input>
+                <input className="contact__input" placeholder="Nom" value={inputSurname} onChange={handleInputSurnameChange}></input>
                 </div>
                 <div className="contact__form-line">
-                <input className="contact__input" placeholder="Adresse mail*" required></input>
-                <input className="contact__input" placeholder="Numero de telephone"></input>
+                <input className="contact__input" placeholder="Adresse mail*" required value={inputAdress} onChange={handleInputAdressChange}></input>
+                <input className="contact__input" placeholder="Numero de telephone" value={inputPhone} onChange={handleInputPhoneChange}></input>
                 </div>
-                <textarea className="contact__input-message" placeholder="Message"></textarea>
+                <textarea className="contact__input-message" placeholder="Message" value={inputMessage} onChange={handleInputMessageChange}></textarea>
                 <p className="contact__note">*Note: necessaire a remplir</p>
                 <button className="contact__submit" type="submit">Envoyer</button>
             </form>
--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -3,35 +3,33 @@ import './Intro.css';
 import SafeBox from "../../images/safebox.jpeg"
 import { Link } from "react-router-dom";
 import  axios from 'axios'
+// import Lala from './Lala'
 
 export default function Intro() {
     const [posts, setPosts] = useState([])
     // const [featuredImage, setFeaturedImage] = useState()
+    // console.log(featuredImage)
 
-    // const fetchPosts = () =>[
-    //     axios
-    //       .get("http://localhost/wordpress/wp-json/wp/v2/posts")
-    //       .then((res) => {
-    //         setPosts(res.data);
-    //       })
-    // ]
+    const fetchPosts = () =>[
+        axios
+          .get("http://localhost/wordpress/wp-json/wp/v2/posts")
+          .then((res) => {
+            setPosts(res.data);
+          })
+    ]
 
-    // const getImage = () => {
-    //     axios
-    //     .get(posts?._links["wp:featuredmedia"][0]?.href)
-    //     .then((res) => {
-    //         setFeaturedImage(res.data.source_url)
-    //     })
-    // }
-    // useEffect(() => {
-    //     getImage()
-    // },[])
-  
-    // useEffect(() => {
-    //     fetchPosts()
-    // }, [])
+    useEffect(() => {
+        fetchPosts()
+    }, [])
 
-    // console.log(posts[1].content.rendered)
+    // console.log(posts[0]._links["wp:featuredmedia"][0].href)
+    // console.log(posts[0].content.rendered)
+
+    // useEffect(({post}) => {
+    //     getImage(post)
+    // },[post])
+
+    // console.log(posts[0])
     
     return(
         <>
@@ -47,12 +45,13 @@ export default function Intro() {
                 </Link>
             </div>
             <img className="intro__picture" src={SafeBox}></img>
+            {/* <img src={'https://wesafe.store/wp-content/uploads/2024/02/logo-banniere-bleu-En.jpg'}></img> */}
             <div>
-                {/* {posts.map((post) =>{
-                    <div>
-                    {post.title.rendered}
-                    </div>
-                })} */}
+                {/* {posts.map((item) => (
+                    // return(
+                     <Lala post={item} />
+                    //  )
+                ))} */}
             </div>
         </section>
         </>
