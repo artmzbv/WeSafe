efbb7157591c3753d20ec6b9cbbf43ce2b0b2b47
Artem Zubov <artmzbv@gmail.com>
2024-03-19 17:09:53 +0300

    quesitons

--- src/components/AboutQuestions/AboutQuestions.jsx
+++ src/components/AboutQuestions/AboutQuestions.jsx
@@ -14,7 +14,7 @@ export default function AboutQuestions() {
             <div className='questions__attention'>
             <h2 className='questions__subtitle questions__subtitle_attention'>Pour votre information</h2>
             <p className='questions__paragraph'>{'Nous ne sommes ni conseillers financiers,\nni conseillers patrimoniaux.'}</p>
-            <p className='questions__paragraph'>{'Notre metier est la transaction\n et le stockage en matériaux critiques.'}</p>
+            <p className='questions__paragraph'>{'Notre métier est la transaction\n et le stockage en matériaux critiques.'}</p>
             </div>
         </section>
         </>
