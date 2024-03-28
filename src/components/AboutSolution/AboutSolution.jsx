56f1d87d7c9478a341905d5272b4e53a32c6f157
Artem Zubov <artmzbv@gmail.com>
2024-03-28 15:08:25 +0300

    chr_jer

--- src/components/AboutSolution/AboutSolution.jsx
+++ src/components/AboutSolution/AboutSolution.jsx
@@ -26,10 +26,10 @@ export default function AboutSolution() {
                         Épargne alternative
                     </li>
                     <li className='solution__item'>
-                        Participation à l'amélioration des filières
+                        {`Participation à l'amélioration\ndes filières`}
                     </li>
                     <li className='solution__item'>
-                        Création d'un stock pour les industriels de France
+                        {`Création d'un stock pour\nles industriels de France`}
                     </li>
                     <li className='solution__item'>
                         Transparence
